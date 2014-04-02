4be9fa3aa2be00adf96895713bd336fa0c026d5b
Sergey <sergeypanic@gmail.com>
2014-04-02 22:05:45 +0400

    Initialized a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BitcoinBot::Application.config.secret_key_base = '1a1a254e064bfb19651bf952f64d96494820ed3da60f0ad1b100ff8ed5bf43a5b6a1f33221b00381cceaa868df008fe006699ad781eae8da57e8cdd843fa625d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BitcoinBot::Application.config.session_store :cookie_store, key: '_bitcoin_bot_session'
