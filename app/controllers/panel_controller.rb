ca8ab9b9631390f6184366f51c6b3242ab17e2b5
Sergey <sergeypanic@gmail.com>
2014-04-04 02:42:39 +0400

    removed comment string

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -2,9 +2,8 @@ class PanelController < ApplicationController
 
 	def index
 		ticker = Btce::Ticker.new "btc_usd"
-		@ticker_json = ticker.json["btc_usd"]
-		# my_hash = JSON.parse('{"hello": "goodbye"}')
-		logger.debug "====================#{@ticker_json}"
+		@ticker_hash = ticker.json["btc_usd"]
+		logger.debug "====================#{@ticker_hash}"
 	end
-	
+
 end
