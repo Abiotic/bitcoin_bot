6287487e52b053ef903dc3043cd4ce3904dad890
Sergey <sergeypanic@gmail.com>
2014-04-05 15:24:22 +0400

    multiple currencys

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,9 +1,14 @@
 class PanelController < ApplicationController
 
 	def index
-		ticker = Btce::Ticker.new "btc_usd"
-		@ticker_hash = ticker.json["btc_usd"]
-		logger.debug "====================#{@ticker_hash}"
+		ticker_usd = Btce::Ticker.new "btc_usd"
+		@btc_usd = ticker_usd.json["btc_usd"]
+
+		ticker_eur = Btce::Ticker.new "btc_eur"
+		@btc_eur = ticker_eur.json["btc_eur"]
+
+		ticker_rur = Btce::Ticker.new "btc_rur"
+		@btc_rur = ticker_rur.json["btc_rur"]		
 	end
 
 end
