7ed31608592d505afe760c91766ae704ca4b1a76
Sergey <sergeypanic@gmail.com>
2014-06-03 15:20:13 +0400

    created logic of currencies vizualization

--- app/assets/javascripts/panel.js
+++ app/assets/javascripts/panel.js
@@ -3,6 +3,17 @@
 
 $(function() {
 
-			
+
+	currencies = JSON.parse(currencies.replace(/&quot;/g,'"'));
+
+	var course_graph_rur = [];
+	var course_graph_eur = [];
+
+	for (i = 0; i < currencies.length; i++) {
+		course_graph_rur.push([i,currencies[i].btc_rur_cource]);
+		course_graph_eur.push([i,currencies[i].btc_eur_cource]);
+	}
+
+	$.plot("#chart", [ course_graph_eur ]);
 
 });
--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -15,7 +15,42 @@ class PanelController < ApplicationController
 		@virx_btc = vircurex["BTC"] # тут создаем переменную экземпляра, чтобы можно было ее юзать во вьюхе index.html. я взял инфу только о BTC (биткоинах)
 		# в response не похожий на btc-e вывод. там нет ни sell ни buy. нужно правильный API запрос сделать. это уже твои знания предметной области должны быть. ты лучше знаешь что запрашивать у биржи
 	
+		currencies = Currency.last(20)
 		
+		
+
+		@cur_to_graph = []
+
+		currencies.each do |currency|
+			cur =  {btc_rur_cource: "", 
+				 	btc_usd_cource: "",
+				 	btc_eur_cource: "",
+					xpm_btc_cource: "",
+				 	ltc_btc_cource: "",
+				 	nmc_btc_cource: "",
+				 	query_time: "",
+				 }
+
+			cur[:btc_rur_cource] = eval(currency.btc_rur_cource)["last"]
+			cur[:btc_usd_cource] = eval(currency.btc_usd_cource)["last"]
+			cur[:btc_eur_cource] = eval(currency.btc_eur_cource)["last"]
+			cur[:xpm_btc_cource] = eval(currency.xpm_btc_cource)["last"]
+			cur[:ltc_btc_cource] = eval(currency.ltc_btc_cource)["last"]
+			cur[:nmc_btc_cource] = eval(currency.nmc_btc_cource)["last"]
+			cur[:query_time] = currency.query_time
+
+			logger.debug "===============#{eval(currency.btc_rur_cource)["last"]}"
+			@cur_to_graph << cur
+			
+		end
+
+		# @cur = @cur.to_json
+		# replace(/&quot;/g,'"')
+		logger.debug "===============#{@cur_to_graph}"
+
+		@cur_to_graph = @cur_to_graph.to_json
+
+
 	end
 
 end
